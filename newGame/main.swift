8dd98082576b60ea0aa6ad09a8cfba102650193c
Bersabeh Asefa <bersabehasefa@interapts-macbook-pro-8.local>
2018-02-13 15:52:01 -0500

    Initial Commit

---
+++ newGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  newGame
+//
+//  Created by Bersabeh Asefa on 2/13/18.
+//  Copyright © 2018 Bersabeh Asefa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
